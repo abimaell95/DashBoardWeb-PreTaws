59b67ccdebed98e9d58bbb2d6275514d9cb4b76a
jjgilces <59465061+jjgilces@users.noreply.github.com>
2020-05-07 21:29:52 -0500

    paises responsives

--- PYTHON/analisis.py
+++ PYTHON/analisis.py
@@ -3,9 +3,11 @@ import numpy as np
 from textblob import TextBlob
 import seaborn as sns
 import matplotlib.pyplot as plt
+import json
 
 df = pd.read_csv("/Users/User/Desktop/proyecto/PYTHON/news_latam.csv", sep=",")
 titulos=df.cnn_prediction.unique()
+paises=df.country.unique()
 
 df['polaridad'] = df['title_trans'].apply(
     lambda x: TextBlob(x).sentiment.polarity)
@@ -35,33 +37,42 @@ for elem in df.country.unique():
 # print(d)
 matriz = np.array(m)
 transpuesta = matriz.transpose()
-ecuadorTotal= np.sum(transpuesta[:, 0:9],axis=0)
-ecuadorPositivo =((transpuesta[:, 0:9][0])*100/ecuadorTotal).astype(int)
-ecuadorNeutro = ((transpuesta[:, 0:9][1])*100/ecuadorTotal).astype(int)
-ecuadorNegativo =((transpuesta[:, 0:9][2])*100/ecuadorTotal).astype(int)
+#ARCHIVO JSON
+feelings=['positivo','neutro','negativo']
+datos={}
+datos['topicos']=list(titulos)
 
-print(ecuadorPositivo)
+N=3 #NUMERO DE PAISES
+F=3 #NUMERO DE SENTIMIENTO
+for i in range(N):
+    matrizPais= transpuesta[:,9*(i):9*(i+1)]
+    totalPais= np.sum(matrizPais,axis=0)
+    datos['total%s'%(paises[i])]=totalPais
+    for j in range(F):
+        fPaisP= (matrizPais[j]*100)/totalPais
+        datos['%s%s'%(paises[i],feelings[j])]=list(np.around(fPaisP,decimals= 3))
+    
+# datosJson= json.dumps(datos)
+# print(type(datosJson))
+print(type(datos))
 
-mexicoPositivo = transpuesta[:, 9:18][0]
-mexicoNeutro = transpuesta[:, 9:18][1]
-mexicoPositivo = transpuesta[:, 9:18][2]
+print(datos)
+        
 
-venezuelaPositivo = transpuesta[:, 18:][0]
-venezuelaNeutro = transpuesta[:, 18:][1]
-venezuelaPositivo = transpuesta[:, 18:][2]
-N = 9
 
-ind = np.arange(N)    # the x locations for the groups
-width = 0.30       # the width of the bars: can also be len(x) sequence
+# N = 9
 
-positivo = plt.bar(ind, ecuadorPositivo, width)
-neutro = plt.bar(ind, ecuadorNeutro, width, bottom=ecuadorPositivo)
-negativo = plt.bar(ind, ecuadorNegativo, width,bottom= ecuadorNeutro+ecuadorPositivo)
+# ind = np.arange(N)    # the x locations for the groups
+# width = 0.30       # the width of the bars: can also be len(x) sequence
 
-plt.ylabel('Scores')
-plt.title('Scores by feelings')
-plt.xticks(ind, tuple([tipo for tipo in titulos]))
-plt.yticks(np.arange(0, 100, step=10))
-plt.legend((positivo[0], neutro[0],negativo[0]), ('Positivo', 'Neutro','Negativo'))
+# positivo = plt.bar(ind, ecuadorPositivo, width)
+# neutro = plt.bar(ind, ecuadorNeutro, width, bottom=ecuadorPositivo)
+# negativo = plt.bar(ind, ecuadorNegativo, width,bottom= ecuadorNeutro+ecuadorPositivo)
 
-plt.show()
+# plt.ylabel('Scores')
+# plt.title('Scores by feelings')
+# plt.xticks(ind, tuple([tipo for tipo in titulos]))
+# plt.yticks(np.arange(0, 100, step=10))
+# plt.legend((positivo[0], neutro[0],negativo[0]), ('Positivo', 'Neutro','Negativo'))
+
+# plt.show()
