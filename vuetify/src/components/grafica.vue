68d3bb0638abc81a4a829b2e129d9d5bc3de5d74
eapb99 <emaalpar@espol.edu.ec>
2020-05-13 18:20:19 -0500

    nada bueno

--- vuetify/src/components/grafica.vue
+++ vuetify/src/components/grafica.vue
@@ -1,69 +1,10 @@
 <template>
-    <body>
-        <canvas id="chart" width="400" height="400"></canvas>
-           
-
-    </body>
-
+    <div id="app">
+        
+    </div>
 </template>
 
+
 <script>
-    
-    var ctx = document.getElementById('chart');
-    var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-
-    const xlabels=[];
-    const xvalues=[];
-
-    const url='https://raw.githubusercontent.com/abimaell95/DashBoardWeb-PreTaws/master/Radar/2015.json'
-        getdata()
-        async function getdata(){
-            const response =await fetch (url)
-            const datos= await response.json()
-            for (let key in datos){
-                xlabels.push(key)
-                xvalues.push(datos[key])
-            }
-            /*const { polit, crime }=datos*/
-            console.log(xvalues)
-            document.getElementById("polit").textContent= xvalues;
-            /*document.getElementById("crime").textContent= "world";*/
-            
-        }
     
 </script>
